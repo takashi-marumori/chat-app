beaa5415a16861b597eaadf2ebb296946d10f9d5
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2020-12-28 11:58:35 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_12_28_003314) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "confirmation_sent_at"
+    t.string "content"
     t.bigint "room_id"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
